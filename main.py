8f2b4bb0b296c3dce3cfc5d8173207b94c0a3089
msy7822-ux <msy780022@gmail.com>
2024-01-14 01:48:37 +0900

    CompVisで画像生成できるように修正

--- main.py
+++ main.py
@@ -1,37 +1,38 @@
-# import torch
-# from torch import autocast
-# from diffusers import StableDiffusionPipeline
+from contextlib import nullcontext
+import torch
+from torch import autocast
+from diffusers import StableDiffusionPipeline
+from dotenv import load_dotenv
+import os
 
-# hugging_token = "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"
+load_dotenv()
 
-# ldm = StableDiffusionPipeline.from_pretrained(
-#     "CompVis/stable-diffusion-v1-4",
-#     variant="fp16",
-#     torch_dtype=torch.float16,
-#     use_auth_token=hugging_token,
-# ).to("cuda")
+TOKEN = os.getenv("HF_TOKEN")
 
-# prompt = "Cyberpunk old man"
+ldm = StableDiffusionPipeline.from_pretrained(
+    "CompVis/stable-diffusion-v1-4",
+    variant="fp16",
+    torch_dtype=torch.float16,
+    use_auth_token=TOKEN,
+).to("mps")
 
-# # 1000枚画像を作りたい場合
-# num_images = 1
-# for j in range(num_images):
-#     with autocast("cuda"):
-#         image = ldm(prompt).images[0]  # 500×500px画像が生成
-#         # 画像サイズを変更したい場合
-#         # image = ldm(prompt, height=400, width=400).images[0]
+prompt = "Cyberpunk old man"
 
-#     # save images (本コードでは、直下に画像が生成されていきます。)
-# image.save(f"./image_{j}.png")
+# 1000枚画像を作りたい場合
+num_images = 1
+for j in range(num_images):
+    with nullcontext("mps"):
+        image = ldm(prompt).images[0]  # 500×500px画像が生成
+        # 画像サイズを変更したい場合
+        # image = ldm(prompt, height=400, width=400).images[0]
+
+    # save images (本コードでは、直下に画像が生成されていきます。)
+image.save(f"./outputs/image_{j}.png")
 
 
 import torch
 from diffusers import DiffusionPipeline
 
-
-MODEL_ID = "CompVis/stable-diffusion-v1-4"
-DEVICE = "cuda"
-YOUR_TOKEN = "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"
 
 pipe = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
 pipe = pipe.to("mps")
